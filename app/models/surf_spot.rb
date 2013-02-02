f01cbe25b9a516c3d977eba0d8924eda4d2bcf46
Bob Smith <bob@ringrevenue.com>
2013-02-01 22:24:39 -0800

    add widget url to model

--- app/models/surf_spot.rb
+++ app/models/surf_spot.rb
@@ -5,4 +5,22 @@ class SurfSpot < ActiveRecord::Base
     SurfSpot.all(:limit=>3)
   end
 
+  def msw_widget_embed
+    msw_widget_embed_template % [ msw_name, msw_id, name ]
+  end
+
+  private
+
+  def msw_widget_embed_template
+'<!-- This code is issued by Magicseaweed.com under license 1359784508_95420 for the website  only subject to terms and conditions
+and this message being kept intact as part of the code. If you are not the license holder add this content to your website by registering at
+Magicseaweed.com. All copyrights retained by Metcentral Ltd and any attempt to modify or redistribute this code is prohibited.
+Please contact us for more information if required. -->
+<div style="width:400px;background:#fff">
+  <script type="text/javascript" src="http://magicseaweed.com/syndicate/index.php?licenseKey=1359784508_95420"></script>
+   <p>
+   <div style="font-family:Arial, Helvetica, sans-serif;text-align:center;font-size:10px;color:#000;height:25px;">
+     <a href="http://magicseaweed.com/%s/%s/" style="color:#000;"> %s Surf Report and Forecast </a> </div> </p> </div>'
+  end
+
 end
--- test/unit/surf_spot_test.rb
+++ test/unit/surf_spot_test.rb
@@ -19,6 +19,36 @@ class SurfSpotTest < ActiveSupport::TestCase
       assert_equal 100,                sp.msw_id
       assert_equal "zuma-surf-report", sp.msw_name
     end
+
+    should "be able to generate the widget url" do
+      sp = SurfSpot.new(
+        :lat=> "100.00",
+        :long => "100.00",
+        :name => "California Street (C Street)",
+        :msw_id => 275,
+        :msw_name => "California-Street-C-Street-Surf-Report"
+      )
+
+
+      expected = '
+        <!-- This code is issued by Magicseaweed.com under license 1359784508_95420 for the website  only subject to terms and conditions
+        and this message being kept intact as part of the code. If you are not the license holder add this content to your website by registering at
+        Magicseaweed.com. All copyrights retained by Metcentral Ltd and any attempt to modify or redistribute this code is prohibited.
+        Please contact us for more information if required. -->
+        <div style="width:400px;background:#fff">
+        <script type="text/javascript" src="http://magicseaweed.com/syndicate/index.php?licenseKey=1359784508_95420"></script>
+        <p>
+        <div style="font-family:Arial, Helvetica, sans-serif;text-align:center;font-size:10px;color:#000;height:25px;">
+        <a href="http://magicseaweed.com/California-Street-C-Street-Surf-Report/275/" style="color:#000;">
+        California Street (C Street) Surf Report and Forecast
+        </a>
+        </div>
+        </p>
+        </div>'
+
+      assert_equal expected.squish, sp.msw_widget_embed.squish
+
+    end
   end
 
 
