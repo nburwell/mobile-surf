b486f5e9d0d2530a42a06413869e0977e99cac5a
Nick Burwell <nburwell@gmail.com>
2013-02-01 20:16:37 -0800

    Rails app with Zurb foundation gem / assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackathon2013b::Application.config.secret_token = '6663b1138e9fa91ca9a974b1bb4a71318b7193183e1ef98c44b409f611b1fb9c34394fdad4612abc803b2786b96219e8325af4ac26f09f81a5fcee14de3c02ca'
